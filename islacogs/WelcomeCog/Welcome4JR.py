6af71ce910d73343125c75cc460df55f11058f92
IslaNub <34550190+IslaNub@users.noreply.github.com>
2018-02-20 17:33:22 +0100

    Update Welcome4JR.py

--- islacogs/WelcomeCog/Welcome4JR.py
+++ islacogs/WelcomeCog/Welcome4JR.py
@@ -12,6 +12,7 @@ class Welcome:
 
     def __init__(self, bot):
         self.bot = bot
+        self.settings = fileIO("data/welcome/settings.json", "load")
 
 
     @commands.group(pass_context=True, no_pm=True)
@@ -35,3 +36,30 @@ async def on_member_join(self, member):
         if r.emoji == emoji[0]:
             pass
         elif r.emoji == emoji[1]:
+
+def check_folders():
+    if not os.path.exists("data/welcome"):
+        print("Creating data/welcome folder...")
+        os.makedirs("data/welcome")
+
+def check_files():
+    f = "data/welcome/settings.json"
+    if not fileIO(f, "check"):
+        print("Creating welcome settings.json...")
+        fileIO(f, "save", {})
+    else: #consistency check
+        current = fileIO(f, "load")
+        for k,v in current.items():
+            if v.keys() != default_settings.keys():
+                for key in default_settings.keys():
+                    if key not in v.keys():
+                        current[k][key] = default_settings[key]
+                        print("Adding " + str(key) + " field to welcome settings.json")
+        fileIO(f, "save", current)
+
+def setup(bot):
+    check_folders()
+    check_files()
+    n = Welcome(bot)
+    bot.add_listener(n.member_join,"on_member_join")
+    bot.add_cog(n)
