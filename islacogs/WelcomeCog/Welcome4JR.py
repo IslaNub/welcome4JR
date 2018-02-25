3ad97d012633726101ecf3393fc431a21cf62c59
IslaNub <34550190+IslaNub@users.noreply.github.com>
2018-02-25 13:07:44 +0100

    Update Welcome4JR.py

--- islacogs/WelcomeCog/Welcome4JR.py
+++ islacogs/WelcomeCog/Welcome4JR.py
@@ -18,7 +18,7 @@ class Welcome:
         numbers = [1, 2, 3, 4]
         for n in numbers:
             emojis.append('{}⃣'.format(str(n)))
-        message = 'Hello!! Welcome to the official Team Liquid discord server. Hope you have a great time here\n1. Want to join our TeamLiquid clans?\n2. Look at our clan list.\n3. Have any questions?\n4. Ask for a role'
+        message = f'Hello {member.mention}! Welcome to the official Team Liquid discord server. Hope you have a great time here\n1. Want to join our TeamLiquid clans?\n2. Look at our clan list.\n3. Have any questions?\n4. Ask for a role'
         channel_id = '414095133294985221'
         m = await self.bot.send_message(self.bot.get_channel(channel_id), message)
         for e in emojis:
@@ -28,18 +28,15 @@ class Welcome:
             r, u = await self.bot.wait_for_reaction(emoji=emojis, user=member, message=m, timeout=480)
         except TypeError:
             await self.bot.clear_reactions(m)
-            await self.bot.edit_message(m, 'Nuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuub.')
         else:
             await self.bot.clear_reactions(m)
-            await self.bot.edit_message(m, 'Nuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuub.')
             if r.emoji == emojis[0]:
-                await self.bot.send_message(u, 'U nub.')
+                await self.bot.send_message(u, '*Clan List*')
+                await self.bot.edit_message(m, f'Sent Clan List in DM to {member.mention}.')
             elif r.emoji == emojis[1]:
                 pass
             elif r.emoji == emojis[2]:
-                message = ('Beep boop.')
-                channel_id = '414095133294985221'
-                await self.bot.send_message(self.bot.get_channel(channel_id), message)
+                await self.bot.edit_message(m, 'Beep Boop.')
             elif r.emoji == emojis[3]:
                 pass
 
