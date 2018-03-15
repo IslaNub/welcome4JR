9c7aae2b274b8a6b02a68a7734312831134746c4
IslaNub <34550190+IslaNub@users.noreply.github.com>
2018-03-15 10:28:02 +0100

    Update Welcome4JRNew.py

--- Welcome4JRNew/Welcome4JRNew.py
+++ Welcome4JRNew/Welcome4JRNew.py
@@ -33,7 +33,7 @@ class Welcome4JR:
             if r.emoji == emojis[0]:
                 c = self.bot.get_channel('389100476630695946')
                 await self.bot.send_message(member, 'Hello and welcome to Team Liquid\'s Mobile empire. Please review our new player pamphlet for an introduction to Team Liquid and information on our mobile teams. If you are currently in the Team Liquid Mobile empire or you would like to join then please fill out our official player registration located https://goo.gl/6kGVPZ - If you have any questions then please join one of our community chats or reach out to a Discord moderator.')
-                await self.bot.send_message(m, f'Sent Google Form in DM to {member.mention}.')
+                await self.bot.send_message(c, f'Sent Google Form in DM to {member.mention}.')
             elif r.emoji == emojis[1]:
                 c = self.bot.get_channel('389100476630695946')
                 MMod = '<@&325720548527308800>'
@@ -60,7 +60,7 @@ class Welcome4JR:
             elif r.emoji == emojis[3]:
                 c = self.bot.get_channel('389100476630695946')
                 ModMail = '<@422438974859116544>'
-                await self.bot.send_message(c, f'{member.mention} if you have any question regarding Liquid send a message in private to {ModMail}.')
+                await self.bot.send_message(c, '1) What games do you play?\n2) In Game Name\n3) What team are you on?\n4) Anything else you would like to add?')
             elif r.emoji == emojis[4]:
                 server = self.bot.get_server('301578535175323658')
                 c = self.bot.get_channel('389100476630695946')
@@ -68,35 +68,35 @@ class Welcome4JR:
                 s = await self.bot.send_message(c, 'I\'m going to ask to you a couple of questions, please just answer in chat.')
                 q1 = await self.bot.send_message(c, '1) What games do you play?')
                 a1 = await self.bot.wait_for_message(author = member)
-                q2 = await self.bot.send_message(c, '2) What\'s your In Game Name?')
-                a2 = await self.bot.wait_for_message(author = member)
-                q3 = await self.bot.send_message(c, '3) What team are you on?')
-                a3 = await self.bot.wait_for_message(author = member)
-                q4 = await self.bot.send_message(c, '4) Anything else you would like to add?')
-                a4 = await self.bot.wait_for_message(author = member)
-                em = discord.Embed()
-                em.title = f'Informations for "{member.name}"'
-                em.description = f'**__Games:__**\n{a1.content}\n\n**__In-Game Name:__**\n{a2.content}\n\n**__Team:__**\n{a3.content}\n\n**__Notes:__**\n{a4.content}'
-                messages = (q1, q2, q3, q4, a1, a2, a3, a4, s)
-                await self.bot.delete_messages(messages)
-                await self.bot.send_message(c, embed = em)
-                await self.bot.add_roles(member, v)
-                await self.bot.send_message(c, f'Added Visitors Role to {member.mention}')
-                if 'Clash Royale' in a1.content or 'clash royale' in a1.content or 'Clash royale' in a1.content:
-                    cr = discord.utils.get(server.roles, name = 'Clash Royale')
-                    await self.bot.add_roles(member, cr)
-                    await self.bot.send_message(c, f'Added Clash Royale Role to {member.mention}')
-                    pass
-                if 'Brawl Stars' in a1.content or 'brawl stars' in a1.content or 'Brawl stars' in a1.content:
-                    bs = discord.utils.get(server.roles, name = 'Brawl Stars')
-                    await self.bot.add_roles(member, bs)
-                    await self.bot.send_message(c, f'Added Brawl Stars Role to {member.mention}')
-                    pass
-                if 'Arena of Valor' in a1.content or 'Arena Of Valor' in a1.content or 'arena of valor' in a1.content or 'Arena of valor' in a1.content:
-                    aov = discord.utils.get(server.roles, name = 'Arena of Valor')
-                    await self.bot.add_roles(member, aov)
-                    await self.bot.send_message(c, f'Added Arena of Valor Role to {member.mention}')
-                    pass
+                #q2 = await self.bot.send_message(c, '2) What\'s your In Game Name?')
+                #a2 = await self.bot.wait_for_message(author = member)
+                #q3 = await self.bot.send_message(c, '3) What team are you on?')
+                #a3 = await self.bot.wait_for_message(author = member)
+                #q4 = await self.bot.send_message(c, '4) Anything else you would like to add?')
+                #a4 = await self.bot.wait_for_message(author = member)
+                #em = discord.Embed()
+                #em.title = f'Informations for "{member.name}"'
+                #em.description = f'**__Games:__**\n{a1.content}\n\n**__In-Game Name:__**\n{a2.content}\n\n**__Team:__**\n{a3.content}\n\n**__Notes:__**\n{a4.content}'
+                #messages = (q1, q2, q3, q4, a1, a2, a3, a4, s)
+                #await self.bot.delete_messages(messages)
+                #await self.bot.send_message(c, embed = em)
+                #await self.bot.add_roles(member, v)
+                #await self.bot.send_message(c, f'Added Visitors Role to {member.mention}')
+                #if 'Clash Royale' in a1.content or 'clash royale' in a1.content or 'Clash royale' in a1.content:
+                    #cr = discord.utils.get(server.roles, name = 'Clash Royale')
+                    #await self.bot.add_roles(member, cr)
+                    #await self.bot.send_message(c, f'Added Clash Royale Role to {member.mention}')
+                    #pass
+                #if 'Brawl Stars' in a1.content or 'brawl stars' in a1.content or 'Brawl stars' in a1.content:
+                    #bs = discord.utils.get(server.roles, name = 'Brawl Stars')
+                    #await self.bot.add_roles(member, bs)
+                    #await self.bot.send_message(c, f'Added Brawl Stars Role to {member.mention}')
+                    #pass
+                #if 'Arena of Valor' in a1.content or 'Arena Of Valor' in a1.content or 'arena of valor' in a1.content or 'Arena of valor' in a1.content:
+                    #aov = discord.utils.get(server.roles, name = 'Arena of Valor')
+                    #await self.bot.add_roles(member, aov)
+                    #await self.bot.send_message(c, f'Added Arena of Valor Role to {member.mention}')
+                    #pass
 def setup(bot):
     n = Welcome4JR(bot)
     bot.add_cog(n)
