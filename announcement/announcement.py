6847c34197a9ff84b23910ef1ba106da2cdae0f2
IslaNub <34550190+IslaNub@users.noreply.github.com>
2018-03-20 09:41:43 +0100

    Update announcement.py

--- announcement/announcement.py
+++ announcement/announcement.py
@@ -22,11 +22,11 @@ class announcement:
     @empire.command(pass_context = True, no_pm = True, name = 'announcement')
     async def empire_announcement(self, ctx):
         u = ctx.message.author
-        c = self.bot.get_channel('423748512396738571')
+
         ea = discord.utils.get(server.roles, name = 'Empire Announcement')
         if ea in u.roles:
             await self.bot.say('You already have this role.')
-        elif ctx.message.channel == c:
+        elif ctx.message.channel.id == '423748512396738571':
             await self.bot.add_roles(u, ea)
             await self.bot.say(f'Added Empire Announcement role to {u.mention}')
         else:
