b88f4e91b79e372a4079e8d02671044cafbba19d
IslaNub <34550190+IslaNub@users.noreply.github.com>
2018-03-20 09:21:41 +0100

    Create announcement.py

---
+++ empire/announcement.py
@@ -0,0 +1,30 @@
+import discord
+from discord.ext import commands
+from .utils.dataIO import fileIO
+from .utils import checks
+from __main__ import send_cmd_help
+import os
+import asyncio
+
+
+class announcement:
+    """Announcement"""
+
+    def __init__(self, bot):
+        self.bot = bot
+        
+    @commands.command(pass_context = True, no_pm = True)
+    async def empire_announcement(self, ctx):
+        u = ctx.message.author
+        ea = discord.utils.get(server.roles, name = 'Empire Announcement')
+        if ea in u.roles:
+            await self.bot.say('You already have this role.)
+        elif ctx.message.channel.id == '423748512396738571':
+            await self.bot.add_roles(u, ea)
+            await self.bot.say(f'Added Empire Announcement role to {u.mention}')
+        else:
+            await self.bot.say('You cannot use this command.')
+            
+def setup(bot):
+    n = announcement(bot)
+    bot.add_cog(n)
