f09dd91af9cf4615c229ee0fc77558205f01ab04
IslaNub <34550190+IslaNub@users.noreply.github.com>
2018-03-23 17:36:29 +0100

    Update liquidwelcome.py

--- liquidwelcome/liquidwelcome.py
+++ liquidwelcome/liquidwelcome.py
@@ -22,6 +22,7 @@ class liquidwelcome:
         s = self.bot.get_server('301578535175323658')
         m = '**{}** has just left the Server, bye bye<:QooBeeConsole:422749739591794688>... {} members left in the Server'
         await self.bot.send_message(c, m.format(member.name, s.member_count))
+        await self.bot.send_message(member, 'kek')
 def setup(bot):
     n = liquidwelcome(bot)
     bot.add_cog(n)
