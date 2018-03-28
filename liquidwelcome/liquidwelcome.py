bd003a5d76c2b0790105363b27c050a7675a71bc
IslaNub <34550190+IslaNub@users.noreply.github.com>
2018-03-28 16:39:53 +0200

    Update liquidwelcome.py

--- liquidwelcome/liquidwelcome.py
+++ liquidwelcome/liquidwelcome.py
@@ -40,7 +40,40 @@ class liquidwelcome:
         em.add_field(name="__GUIDE__", value="**Here is a brief guide to our server to get you started:**\n\n[Team Liquid Mobile New Member Pamphlet](https://bit.ly/2px9czy)\n[Team Liquid Mobile New Member Registration](https://goo.gl/6kGVPZ)\n\n[Team Liquid Mobile Academy Team Pamphlet](https://bit.ly/2uk38PI)\n[Team Liquid Mobile Academy Team Registration](https://bit.ly/2G7YTbA)")
         em.set_footer(text="Thanks for joining Liquid")
         await self.bot.say(embed=em)
-
+        
+    @commands.command(pass_context=True, no_pm=True)
+    async def who(self, ctx):
+        """Find out who is stupid!"""
+        channel_id = ctx.message.channel.id
+        owner = ctx.message.author
+        message = 'Who is stupid?'
+        m = await self.bot.send_message(self.bot.get_channel(channel_id), message)
+        ms = await self.bot.wait_for_message(check = lambda x : x.author == ctx.message.author)
+        IslaNub = '199436790581559296'
+        saka = '323543378509824002'
+        isla = '<@199436790581559296>'
+        if ctx.message.author.id == IslaNub:
+            await self.bot.say(f'Oh, my Master! You\'re the brightest person I\'ve ever seen! You definitely are right! {ms.content} really is stupid!')
+        elif ctx.message.author.id == saka:
+            await self.bot.say(f'Awww saka, since Isla has faith in you I\'m pretty sure you\'re right, {ms.content} is pretty much stupid!')
+        elif ms.content == isla:
+            await self.bot.say('Idk')
+        else:
+            await self.bot.say('Are you sure? (yes/no)')
+            answer1 = ('yes')
+            answer2 = ('no')
+            msg = await self.bot.wait_for_message(author=owner)
+            if msg.content.lower().strip() in answer1:
+                if ms.content == 'I':
+                    await self.bot.send_message(self.bot.get_channel(channel_id), 'Indeed, you are! Btw it\'s "me", not "I"...')
+                elif ms.content == 'You':
+                    await self.bot.send_message(self.bot.get_channel(channel_id), 'I am smart enough to understand you tried to troll me... Believe me, the stupid here is you, not me!')
+                else:
+                    await self.bot.send_message(self.bot.get_channel(channel_id), f'Hmm perhaps, I\'m not sure if {ms.content} is stupid, but I\'m sure YOU are!')
+            elif msg.content.lower().strip() in answer2:
+                await self.bot.send_message(self.bot.get_channel(channel_id), 'Nice! You\'re right, because you\'re stupid!')
+            else:
+                await self.bot.send_message(self.bot.get_channel(channel_id), 'What don\'t you understand of "yes/no"??? Look how stupid...')
 def setup(bot):
     n = liquidwelcome(bot)
     bot.add_cog(n)
