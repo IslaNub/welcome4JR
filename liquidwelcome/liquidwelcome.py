e93f92c9ca1511fcd873abd71c1e9e5429b97235
IslaNub <34550190+IslaNub@users.noreply.github.com>
2018-03-29 18:54:43 +0200

    Update liquidwelcome.py

--- liquidwelcome/liquidwelcome.py
+++ liquidwelcome/liquidwelcome.py
@@ -16,14 +16,14 @@ class liquidwelcome:
         c = self.bot.get_channel('389100476630695946')
         m = 'Hello {}! Welcome to the Official Team Liquid Mobile Discord server. \nPlease join a chat channel and add the role for the game you play (+ClashRoyale, +AoV, +BrawlStars).\nYou must be a member in one of our official teams to have a member role.\nYou must be an elder, co-leader, or leader in one of our official teams to have a citizen role. \nCheck out our various channels for info on prizes, events, teams, and much more. \nTag an online moderator or DM Liquid Mail if you have any questions.'.format(member.mention)
         await self.bot.send_message(c, m)
-        m2 ='Hello and welcome to Team Liquid\'s Mobile empire. Please review our new member pamphlet @ https://bit.ly/2px9czy for an introduction to Team Liquid and information on our mobile teams. \n\nIf you are currently in the Team Liquid Mobile empire or you would like to join then please fill out our official member registration located @ https://goo.gl/6kGVPZ - \n\nIf you have any questions then please join one of our community chats or reach out to a Discord moderator'
-        await self.bot.send_message(member, m2)
+    #   m2 ='Hello and welcome to Team Liquid\'s Mobile empire. Please review our new member pamphlet @ https://bit.ly/2px9czy for an introduction to Team Liquid and information on our mobile teams. \n\nIf you are currently in the Team Liquid Mobile empire or you would like to join then please fill out our official member registration located @ https://goo.gl/6kGVPZ - \n\nIf you have any questions then please join one of our community chats or reach out to a Discord moderator'
+    #   await self.bot.send_message(member, m2)
         
-    async def on_member_remove(self, member):
-        c = self.bot.get_channel('414094090070786058')
-        s = self.bot.get_server('301578535175323658')
-        m = '**{}** has just left the Server, bye bye<:QooBeeConsole:422749739591794688>... {} members left in the Server'.format(member.name, s.member_count)
-        await self.bot.send_message(c, m)
+    #async def on_member_remove(self, member):
+    #    c = self.bot.get_channel('414094090070786058')
+    #    s = self.bot.get_server('301578535175323658')
+    #    m = '**{}** has just left the Server, bye bye<:QooBeeConsole:422749739591794688>... {} members left in the Server'.format(member.name, s.member_count)
+    #    await self.bot.send_message(c, m)
         
     @commands.command(pass_context = True, no_pm = True)
     async def liquid(self, ctx):
