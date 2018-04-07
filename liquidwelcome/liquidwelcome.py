45e02a796eb584a763ac34e9a7fae836afd3eb37
IslaNub <34550190+IslaNub@users.noreply.github.com>
2018-04-07 13:16:14 +0200

    Update liquidwelcome.py

--- liquidwelcome/liquidwelcome.py
+++ liquidwelcome/liquidwelcome.py
@@ -13,7 +13,7 @@ class liquidwelcome:
         self.bot = bot
 
     async def on_member_join(self, ctx, member):
-        nv = discord.utils.get(ctx.message.server.roles, name = 'Not Verified')
+        nv = discord.utils.get(message.server.roles, name = 'Not Verified')
         await self.bot.add_roles(nv, member)
         c = self.bot.get_channel('389100476630695946')
         m = '**Hello {}! Welcome to the Official Team Liquid Mobile Discord server.**\n\n<:tl:429889965397245964>Please look in <#429719437763936256> and add the role for the game you play.\n<:tl:429889965397245964>You must be a member in one of our official teams to have a member role.\n<:tl:429889965397245964>Check out our various channels for info on prizes, events, teams, and much more.\n<:tl:429889965397245964>Tag an online moderator or DM Liquid Mail if you have any questions. By clicking on the "✅" reaction you will get access to the Server and you declare that you have read the Server rules.'.format(member.mention)
