32f036935cc1c8b8673b0b8d3005cf6d95306e5a
IslaNub <34550190+IslaNub@users.noreply.github.com>
2018-04-17 21:49:43 +0200

    Update liquidwelcome.py

--- liquidwelcome/liquidwelcome.py
+++ liquidwelcome/liquidwelcome.py
@@ -139,9 +139,8 @@ class liquidwelcome:
         m = await self.bot.get_message(c, ID)
         try:
             r = await self.bot.get_reaction_users(discord.Reaction(emoji = '✅', message = m))
-            u = discord.utils.get(ctx.message.server.members, ctx.message.server.get_member(r))
-                                
-            await self.bot.say(u)
+            for discord.User in r:
+                await self.bot.say(r)
         except Exception as e:
             await self.bot.say(e)
             print(e)
