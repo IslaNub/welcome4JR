4d1d01619ba3957725b6ab9322c3442671048d3b
IslaNub <34550190+IslaNub@users.noreply.github.com>
2018-05-07 15:06:03 +0200

    Update liquidwelcome.py

--- liquidwelcome/liquidwelcome.py
+++ liquidwelcome/liquidwelcome.py
@@ -289,12 +289,16 @@ class liquidwelcome:
             
     def getAuth(self):
         return {'auth' : '2da0f327dd7f41c7b0d87fae844fc3f24bc7c9ad99d44a7b9bc61f9cd76600dd'}
+    
+APIAuth = {
+'auth': "2da0f327dd7f41c7b0d87fae844fc3f24bc7c9ad99d44a7b9bc61f9cd76600dd"
+}
             
     @commands.command(pass_context = True, no_pm = True)
     async def academyclan(self, ctx):
         academy = 'P2GJGRUY'
         try:
-            headers = self.getAuth
+            headers = APIAuth
             url = "https://api.royaleapi.com/clan/{}".format(academy)
             async with aiohttp.ClientSession() as session:
                 async with session.get(url, headers=headers) as resp:
