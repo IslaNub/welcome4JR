4e7e2680d4a004c268dd90262e9f9e77db46eba8
IslaNub <34550190+IslaNub@users.noreply.github.com>
2018-05-07 14:15:27 +0200

    Update liquidwelcome.py

--- liquidwelcome/liquidwelcome.py
+++ liquidwelcome/liquidwelcome.py
@@ -318,7 +318,7 @@ class liquidwelcome:
         data = response.json()
         try:
             embed = discord.Embed(title = '', url = 'https://royaleapi.com/clan/P2GJGRUY')
-            embed.set_author(name = 'Stats for Liquid Academy!, color = 0x00FFBF)
+            embed.set_author(name = 'Stats for Liquid Academy!', color = 0x00FFBF)
             embed.add_field(name = data['name'] + ' (#' + data['tag'] + ') Stats!', inline = True)
             embed.add_field(name = 'Description:', value = data['description'], inline = True)
             await self.bot.say(embed = embed)
