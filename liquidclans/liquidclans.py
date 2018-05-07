d1c5588c5e3f35c639fd1f0b43d398285ed2f087
IslaNub <34550190+IslaNub@users.noreply.github.com>
2018-05-07 12:21:41 +0200

    Update liquidclans.py

--- liquidclans/liquidclans.py
+++ liquidclans/liquidclans.py
@@ -1,6 +1,29 @@
+import discord
+from discord.ext import commands
+from .utils.dataIO import fileIO
+from .utils import checks
+from __main__ import send_cmd_help
+from random import choice
+from random import randint
+import asyncio
 import aiohttp
-headers = {"auth": "a123b4567"}
-url = "https://api.royaleapi.com/clan/2CCCP"
-async with aiohttp.ClientSession() as session:
-    async with session.get(url, headers=headers) as resp:
-        data = await resp.json()
+
+class liquidwelcome:
+    """Welcomes new members to Team Liquid Mobile"""
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.info_data = data.get('info')
+        
+    @property
+    def clan(self):
+        """Clan."""
+        return self.info_data.get("clan", None)
+
+    @commands.command(pass_context = True, no_pm = True)
+    async def clan(self, ctx, clan_id):
+        headers = {"auth": "2da0f327dd7f41c7b0d87fae844fc3f24bc7c9ad99d44a7b9bc61f9cd76600dd"}
+        url = "https://api.royaleapi.com/clan/{}".format(clan_id)
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, headers=headers) as resp:
+                data = await resp.json()
