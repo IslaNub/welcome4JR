652a499864e63aaf1a99c95a768a8652313122f0
IslaNub <34550190+IslaNub@users.noreply.github.com>
2018-05-08 08:41:27 +0200

    Update liquidwelcome.py

--- liquidwelcome/liquidwelcome.py
+++ liquidwelcome/liquidwelcome.py
@@ -324,7 +324,44 @@ class liquidwelcome:
         except Exception as e:
             await self.bot.say(e) 
             print(e)
+     
+    LiquidClanWhiteList = ['98R22PLY',' 98PYR0VJ', '9UP2JY2P', 'P0YJ0P2V', '8J0J2RQC', '9QRUO2GR', 'P888QQQ9']
             
+    @commands.command(pass_context = True, no_pm = True)
+    async def euclanbeta(self, ctx, clan_tag):
+        while True:
+            x = 0
+            clan = LiquidClanWhiteList[x]
+            try:
+                headers = APIAuth
+                url = "https://api.royaleapi.com/clan/{}".format(clan)
+                async with aiohttp.ClientSession() as session:
+                    async with session.get(url, headers=headers) as resp:
+                        data = await resp.json()
+                        try:
+                            embed = discord.Embed(title = '', url = 'https://royaleapi.com/clan/{}'.format(clan), color = 0x00FFBF)
+                            embed.set_author(name = 'Stats for {}!'.format(data['name']))
+                            embed.title = f"{data['name']} (#{data['tag']})"
+                            embed.set_thumbnail(url = data['badge']['image'])
+                            embed.add_field(name = 'Description:', value = data['description'], inline = True)
+                            embed.add_field(name = 'Clan Points :', value = f"{data['score']} <:Trophy:443281867316264960>", inline = True)
+
+                            embed.add_field(name = 'Member Count:', value = f"{data['memberCount']}/50 <:Members:443282536764801026>", inline = True)
+                            embed.add_field(name = 'Required Trophies:', value = f"{data['requiredScore']} <:Trophy:443281867316264960>", inline = True)
+                            embed.add_field(name = 'Donations:', value = f"{data['donations']} <:Cards:443285942875193344>", inline = True)
+                            embed.add_field(name = 'Type:', value = f"{data['type']}".capitalize(), inline = True)
+                            embed.set_footer(text = 'API powered by RoyaleAPI', icon_url = 'https://raw.githubusercontent.com/cr-api/cr-api-docs/master/docs/img/cr-api-logo-b.png')
+                            embed.add_field(name = 'Location:', value = data['location']['name'], inline = True)
+                            await self.bot.say(embed = embed)
+                        except Exception as e:
+                            await self.bot.say(e)
+                            await self.bot.say('Something went wrong, please try again later.')
+                            print(e)
+            except Exception as e:
+                await self.bot.say(e) 
+                print(e)
+            x += 1
+        
 def setup(bot):
     n = liquidwelcome(bot)
     bot.add_cog(n)
