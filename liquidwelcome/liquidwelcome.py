428e9af11f469b498e305e720322f89e5594a318
IslaNub <34550190+IslaNub@users.noreply.github.com>
2018-05-11 23:06:22 +0200

    Update liquidwelcome.py

--- liquidwelcome/liquidwelcome.py
+++ liquidwelcome/liquidwelcome.py
@@ -425,10 +425,10 @@ class liquidwelcome:
             async with session.get(url, headers=headers) as resp:
                 data = await resp.json()
                 x = 0
-                m = await self.bot.say(f"{data['members'][x]['rank']}. {data['members'][x]['name']} #{data['members'][x]['tag']}")
+                m = await self.bot.say(f"```{data['members'][x]['rank']}. {data['members'][x]['name']} #{data['members'][x]['tag']}```")
                 while True:
                     try:
-                        m = await self.bot.edit_message(m, f"{m.content}\n{data['members'][x]['rank']}. {data['members'][x]['name']} #{data['members'][x]['tag']}")
+                        m = await self.bot.edit_message(m, f"```{m.content}\n{data['members'][x]['rank']}. {data['members'][x]['name']} #{data['members'][x]['tag']}```")
                         x += 1
                     except Exception:
                         break
