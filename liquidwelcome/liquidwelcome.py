4411bb00e7a866a4ccf6cf052d6b40e47e3ef68a
IslaNub <34550190+IslaNub@users.noreply.github.com>
2018-05-18 17:39:11 +0200

    LiquidClans v0.2.2.6b
    I can't use f string or format... :/

--- liquidwelcome/liquidwelcome.py
+++ liquidwelcome/liquidwelcome.py
@@ -9,7 +9,7 @@ import asyncio
 import requests
 import aiohttp
 
-version = '0.2.2.5b'
+version = '0.2.2.6b'
 
 APIAuth = {'auth': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTQ3LCJpZGVuIjoiMTk5NDM2NzkwNTgxNTU5Mjk2IiwibWQiOnt9LCJpYXQiOjE1MjYwMzEzMDd9.5caUvHM82sv4OZ7mxFsnZ20OZwx36QLGoJO93zMDBDQ'}
 EUClans = ['98R22PLY', '98PYR0VJ', '9UP2JY2P', 'P0YJ0P2V', '8J0J2RQC', '9QRUO2GR', 'P888QQQ9']
@@ -313,7 +313,7 @@ class liquidwelcome:
         """Choose between EU, NA and LA
         
         
-        LiquidClans v0.2.2.5b
+        LiquidClans v0.2.2.6b
         NOTE: This is a beta function, the code is still under development."""
         x = 0
         while True:
