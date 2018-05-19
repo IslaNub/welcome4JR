e96090be7979e2106afdf2c46f23107ddc436999
IslaNub <34550190+IslaNub@users.noreply.github.com>
2018-05-20 00:09:07 +0200

    Update liquidclans.py

--- liquidclans/liquidclans.py
+++ liquidclans/liquidclans.py
@@ -35,7 +35,7 @@ class liquidclans:
         
         #await self.bot.say(len(eu))
         c = self.bot.get_channel('447519506210750474')
-        
+        await self.bot.send_message(c, '***__EU CLANS:__***')
         while True:
             try:
                 while True:
