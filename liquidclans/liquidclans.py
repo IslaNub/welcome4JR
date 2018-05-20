4df46b51073b4f438ca86d9d2d5db2c64c3842e0
IslaNub <34550190+IslaNub@users.noreply.github.com>
2018-05-20 16:25:53 +0200

    Update liquidclans.py

--- liquidclans/liquidclans.py
+++ liquidclans/liquidclans.py
@@ -32,7 +32,7 @@ class liquidclans:
     async def triggereu(self, ctx):
         eu = ['98R22PLY', '98PYR0VJ']
         x = 0
-        
+        await self.bot.say('***__EU CLANS:__***')
         #await self.bot.say(len(eu))
         c = self.bot.get_channel('447519506210750474')
         await self.bot.send_message(c, '***__EU CLANS:__***')
@@ -66,12 +66,12 @@ class liquidclans:
                                         #await self.bot.send_message(c, 'Working')
                                         await asyncio.sleep(10)
                                         lim = len(EUClans) + 1
-                                        async for message in self.bot.logs_from(c, limit = lim, reverse = True):
+                                        async for message in self.bot.logs_from(c, limit = lim, reverse = True, before = msg):
                                             to_delete = [] 
                                             to_delete.append(message)
                                             x = 0
                                             await self.mass_purge(to_delete)
-                                            await self.bot.send_message(c, '***__EU CLANS:__***')
+                                            #await self.bot.send_message(c, '***__EU CLANS:__***')
                                             pass
                                         pass
                                     
