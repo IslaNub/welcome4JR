530d44f32602ea3f2983896c1bc0472bc07c2018
IslaNub <34550190+IslaNub@users.noreply.github.com>
2018-05-24 20:17:34 +0200

    Update liquidclans.py

--- liquidclans/liquidclans.py
+++ liquidclans/liquidclans.py
@@ -213,8 +213,7 @@ class liquidclans:
                     async with session.get(url, headers=headers) as resp:
                         data = await resp.json()
                         if data['stats']['challengeMaxWins'] >= 15:
-                            pass                        
-                            #await self.bot.say('**' + data['name'] + ' (#' + str(data['tag']) + ')** has more than 15 Wins.')
+                            pass
                         elif data['stats']['challengeMaxWins'] < 15:
                             await self.bot.say('**' + data['name'] + ' (#' + str(data['tag']) + ')** does NOT have more than 15 Wins.')                       
                         x += 1
@@ -222,7 +221,6 @@ class liquidclans:
                             await self.bot.say('I should have finished...🤔')                     
                             break                            
             except Exception as e:
-                break                                    
                 await self.bot.say(e)
                 break
                                                     
